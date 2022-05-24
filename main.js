80e7306168b2d766a1a490f8aa82582a45237632
Angelov-digital <adamangelo.osvc@gmail.com>
2022-05-24 18:52:40 +0200

    season wheel session

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
-function bounceBox() {
-    let element = document.querySelector('section')
+function changeImage1() {
+    var image = document.getElementById('testimage');
+    if (image.src.match("spring")) {
+        image.src = "img/spring.png";
+    }
+    else {
+        image.src = "img/spring.png";
+    }
+}
 
-    element.classList.toggle()
+function changeImage2() {
+    var image = document.getElementById('testimage');
+    if (image.src.match("summer")) {
+        image.src = "img/summer.png";
+    }
+    else {
+        image.src = "img/summer.png";
+    }
+}
+
+function changeImage3() {
+    var image = document.getElementById('testimage');
+    if (image.src.match("autum")) {
+        image.src = "img/autum.png";
+    }
+    else {
+        image.src = "img/autum.png";
+    }
+}
+
+function changeImage4() {
+    var image = document.getElementById('testimage');
+    if (image.src.match("winter")) {
+        image.src = "img/winter.png";
+    }
+    else {
+        image.src = "img/winter.png";
+    }
 }

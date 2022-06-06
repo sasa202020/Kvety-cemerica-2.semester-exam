a6cc84adc001df64773f6fdf136ea3bf04495508
sasa202020 <sosiso4ki64@gmail.com>
2022-06-06 19:22:23 +0200

    Mobile version improvement
    Add - desktop fix

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 //CAROUSEL thanks goes to https://www.youtube.com/watch?v=KtsVC_JRxCU&ab_channel=codefoxx
 const images = ["img/bouquet1.jpg", "img/bouquet2.jpg", "img/bouquet3.jpg"];
 const carousel = document.querySelector(".carousel");
-const interval = setInterval(function() {
-   startCarousel();
- }, 3000);
+const interval = setInterval(function () {
+  startCarousel();
+}, 3000);
 var index = 1;
 
 startCarousel = () => {
@@ -11,86 +11,82 @@ startCarousel = () => {
   carousel.classList.remove("fade");
   void carousel.offsetWidth;
   carousel.classList.add("fade");
-  if(index > images.length - 1) index = 0;
-}
+  if (index > images.length - 1) index = 0;
+};
 //CArousel end
 //changing images on click
 function changeImage1() {
-    var image = document.getElementById('testimage');
-    if (image.src.match("spring")) {
-        image.src = "img/spring.png";
-    }
-    else {
-        image.src = "img/spring.png";
-    }
+  var image = document.getElementById("testimage");
+  if (image.src.match("spring")) {
+    image.src = "img/spring.png";
+  } else {
+    image.src = "img/spring.png";
+  }
 }
 
 function changeImage2() {
-    var image = document.getElementById('testimage');
-    if (image.src.match("summer")) {
-        image.src = "img/summer.png";
-    }
-    else {
-        image.src = "img/summer.png";
-    }
+  var image = document.getElementById("testimage");
+  if (image.src.match("summer")) {
+    image.src = "img/summer.png";
+  } else {
+    image.src = "img/summer.png";
+  }
 }
 
 function changeImage3() {
-    var image = document.getElementById('testimage');
-    if (image.src.match("autum")) {
-        image.src = "img/autum.png";
-    }
-    else {
-        image.src = "img/autum.png";
-    }
+  var image = document.getElementById("testimage");
+  if (image.src.match("autum")) {
+    image.src = "img/autum.png";
+  } else {
+    image.src = "img/autum.png";
+  }
 }
 
 function changeImage4() {
-    var image = document.getElementById('testimage');
-    if (image.src.match("winter")) {
-        image.src = "img/winter.png";
-    }
-    else {
-        image.src = "img/winter.png";
-    }
+  var image = document.getElementById("testimage");
+  if (image.src.match("winter")) {
+    image.src = "img/winter.png";
+  } else {
+    image.src = "img/winter.png";
+  }
 }
-// choose us 3 ailes 
+// choose us 3 ailes
 function chooseUs(id) {
-    var a = document.getElementById('1');
-    var b = document.getElementById('2');
-    var c = document.getElementById('3');
+  var a = document.getElementById("1");
+  var b = document.getElementById("2");
+  var c = document.getElementById("3");
 
   a.style.display = "none";
   b.style.display = "none";
   c.style.display = "none";
 
   var x = document.getElementById(id);
-    x.style.display = "";
+  x.style.display = "";
 }
-function hideOptions(){
-    var b = document.getElementById('2');
-    var c = document.getElementById('3');
+function hideOptions() {
+  var b = document.getElementById("2");
+  var c = document.getElementById("3");
 
-    b.style.display = "none";
-    c.style.display = "none";
+  b.style.display = "none";
+  c.style.display = "none";
 
-    var w = document.getElementById('5');
-    var e = document.getElementById('6');
-  
-    w.style.display = "none";
-    e.style.display = "none";
+  var w = document.getElementById("5");
+  var e = document.getElementById("6");
+
+  w.style.display = "none";
+  e.style.display = "none";
 }
 // Choose us mobile slider
 function chooseUsMobileSlider(id) {
-  var q = document.getElementById('4');
-  var w = document.getElementById('5');
-  var e = document.getElementById('6');
+  var q = document.getElementById("4");
+  var w = document.getElementById("5");
+  var e = document.getElementById("6");
 
-q.style.display = "none";
-w.style.display = "none";
-e.style.display = "none";
+  q.style.display = "none";
+  w.style.display = "none";
+  e.style.display = "none";
 
-var y = document.getElementById(id);
+  var y = document.getElementById(id);
   y.style.display = "";
 }
 //scroll down button                thanks to https://css-tricks.com/how-to-make-an-unobtrusive-scroll-to-top-button/
@@ -98,41 +94,43 @@ var scrollToTopBtn = document.getElementById("downButton");
 var rootElement = document.documentElement;
 
 function scrollToTop() {
- 
   rootElement.scrollTo({
     top: 620,
-    behavior: "smooth"
+    behavior: "smooth",
   });
 }
 scrollToTopBtn.addEventListener("click", scrollToTop);
 
-
 // presentation slider              thanks to https://www.w3schools.com/howto/howto_js_slideshow.asp
- 
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("slider-cell");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }

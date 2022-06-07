9611f617722af61804ed130374ad5f38f216b70c
sasa202020 <sosiso4ki64@gmail.com>
2022-06-07 23:05:52 +0200

    added stuff

--- main.js
+++ main.js
@@ -1,80 +1,77 @@
 //changing images on click
 function changeImage1() {
-    var image = document.getElementById('testimage');
-    if (image.src.match("spring")) {
-        image.src = "img/spring.png";
-    }
-    else {
-        image.src = "img/spring.png";
-    }
+  var image = document.getElementById("testimage");
+  if (image.src.match("spring")) {
+    image.src = "img/spring.png";
+  } else {
+    image.src = "img/spring.png";
+  }
 }
 
 function changeImage2() {
-    var image = document.getElementById('testimage');
-    if (image.src.match("summer")) {
-        image.src = "img/summer.png";
-    }
-    else {
-        image.src = "img/summer.png";
-    }
+  var image = document.getElementById("testimage");
+  if (image.src.match("summer")) {
+    image.src = "img/summer.png";
+  } else {
+    image.src = "img/summer.png";
+  }
 }
 
 function changeImage3() {
-    var image = document.getElementById('testimage');
-    if (image.src.match("autum")) {
-        image.src = "img/autum.png";
-    }
-    else {
-        image.src = "img/autum.png";
-    }
+  var image = document.getElementById("testimage");
+  if (image.src.match("autum")) {
+    image.src = "img/autum.png";
+  } else {
+    image.src = "img/autum.png";
+  }
 }
 
 function changeImage4() {
-    var image = document.getElementById('testimage');
-    if (image.src.match("winter")) {
-        image.src = "img/winter.png";
-    }
-    else {
-        image.src = "img/winter.png";
-    }
+  var image = document.getElementById("testimage");
+  if (image.src.match("winter")) {
+    image.src = "img/winter.png";
+  } else {
+    image.src = "img/winter.png";
+  }
 }
-// choose us 3 ailes 
+// choose us 3 ailes
 function chooseUs(id) {
-    var a = document.getElementById('1');
-    var b = document.getElementById('2');
-    var c = document.getElementById('3');
+  var a = document.getElementById("1");
+  var b = document.getElementById("2");
+  var c = document.getElementById("3");
 
   a.style.display = "none";
   b.style.display = "none";
   c.style.display = "none";
 
   var x = document.getElementById(id);
-    x.style.display = "";
+  x.style.display = "";
 }
-function hideOptions(){
-    var b = document.getElementById('2');
-    var c = document.getElementById('3');
+function hideOptions() {
+  var b = document.getElementById("2");
+  var c = document.getElementById("3");
 
-    b.style.display = "none";
-    c.style.display = "none";
+  b.style.display = "none";
+  c.style.display = "none";
 
-    var w = document.getElementById('5');
-    var e = document.getElementById('6');
-  
-    w.style.display = "none";
-    e.style.display = "none";
+  var w = document.getElementById("5");
+  var e = document.getElementById("6");
+
+  w.style.display = "none";
+  e.style.display = "none";
 }
+
 // Choose us mobile slider
 function chooseUsMobileSlider(id) {
-  var q = document.getElementById('4');
-  var w = document.getElementById('5');
-  var e = document.getElementById('6');
+  var q = document.getElementById("4");
+  var w = document.getElementById("5");
+  var e = document.getElementById("6");
 
-q.style.display = "none";
-w.style.display = "none";
-e.style.display = "none";
+  q.style.display = "none";
+  w.style.display = "none";
+  e.style.display = "none";
 
-var y = document.getElementById(id);
+  var y = document.getElementById(id);
   y.style.display = "";
 }
 //scroll down button                thanks to https://css-tricks.com/how-to-make-an-unobtrusive-scroll-to-top-button/
@@ -82,42 +79,67 @@ var scrollToTopBtn = document.getElementById("downButton");
 var rootElement = document.documentElement;
 
 function scrollToTop() {
- 
   rootElement.scrollTo({
     top: 620,
-    behavior: "smooth"
+    behavior: "smooth",
   });
 }
 scrollToTopBtn.addEventListener("click", scrollToTop);
 
-
 // presentation slider              thanks to https://www.w3schools.com/howto/howto_js_slideshow.asp
- 
+
 let slideIndex = 1;
-showSlides(slideIndex);
+let pSlideIndex = 1;
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide(n, sName) {
+  showSlides((slideIndex = n), sName);
 }
 
-function showSlides(n) {
-  let slides = document.getElementsByClassName("mySlides");
+function showSlides(n, sName) {
+  let slides = document.getElementsByClassName(sName);
   let dots = document.getElementsByClassName("slider-cell");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function currentPresetSlide(n) {
+  showPresetSlides((pSlideIndex = n));
+}
+
+function showPresetSlides(n) {
+  let pSlides = document.getElementsByClassName("presetDesktopSlides");
+  let options = document.getElementsByClassName("presetOption");
+  if (n > pSlides.length) {
+    pSlideIndex = 1;
+  }
+  if (n < 1) {
+    pSlideIndex = pSlides.length;
+  }
+  for (i = 0; i < pSlides.length; i++) {
+    pSlides[i].style.display = "none";
+  }
+  for (i = 0; i < options.length; i++) {
+    options[i].className = options[i].className.replace("activeHover", "");
+  }
+  pSlides[pSlideIndex - 1].style.display = "block";
+  options[pSlideIndex - 1].className += " activeHover";
 }
 // 1) putting in var all sliders 2) getting slider cells elements 3)
--- presetPreview.js
+++ presetPreview.js
@@ -1,4 +1,17 @@
 //Preset preview slideshow  thanks goes again to https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_slideshow_dots2
+
+//displays only the first element from slides onLoad
+function hideOptionsPreset() {
+  let slides1 = document.getElementsByClassName("presetDesktopSlides");
+  let slides2 = document.getElementsByClassName("bouquetePart");
+
+  for (i = 0; i > slides1.length; i++) {
+    slides1[i].style.display = "none";
+  }
+  for (i = 0; 1 > slides2.length; i++) {
+    slides2[i].style.display = "none";
+  }
+}
 var slideIndex2 = 1;
 showDivs(slideIndex2);
 
@@ -9,7 +22,7 @@ function currentDiv(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides2");
-  var dots = document.getElementsByClassName("demo");
+  var dots = document.getElementsByClassName("slider-cell");
   if (n > x.length) {
     slideIndex2 = 1;
   }
